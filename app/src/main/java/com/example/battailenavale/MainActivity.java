4a2eb7f3de10847820ed2ea087ca28ff1e5ada2d
Jeremy Cotigny <jeremy.cotigny@pole-emploi.fr>
2019-12-07 14:53:16 +0100

    ajout commentaire pour paulin

--- app/src/main/java/com/example/battailenavale/MainActivity.java
+++ app/src/main/java/com/example/battailenavale/MainActivity.java
@@ -11,4 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    // il faut faire comme ça paulin
 }
